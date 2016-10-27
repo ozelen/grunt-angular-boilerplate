c259ea330b680a6654023fbcf128192c0e43336e
Myroslava Pabyrivska <pabyrivska@gmail.com>
2016-10-27 18:00:46 +0300

    test data format

--- tests/directives/ChampList.unit.js
+++ tests/directives/ChampList.unit.js
@@ -14,7 +14,7 @@ describe('Champ List Directive', function () {
 
     scope.competitions = [
       {id:1,caption: 'Premier League',numberOfTeams:20,lastUpdated: 'today'},
-      {id:2,caption: 'Bundesliga',numberOfTeams:20,lastUpdated: 'yesterday'},
+      {id:2,caption: 'Bundesliga',numberOfTeams:20,lastUpdated: '2016-06-15T08:09:51Z'},
     ];
 
     element = $compile('<champ-list competitions=competitions>')(scope);
@@ -25,6 +25,10 @@ describe('Champ List Directive', function () {
   it('should contain field name', function() {
     expect(element.find('td').text()).toContain('Premier League');
     expect(element.find('td').text()).toContain('Bundesliga');
+  });
+
+  it('should contain formatted data', function() {
+    expect(element.find('td').text()).toContain('15.06.2016');
   });
 
 });
